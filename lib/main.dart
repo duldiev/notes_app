b8c8452998cd83abec705c6fb4b5cc789b354e71
Ray <44358562+duldiev@users.noreply.github.com>
2023-08-14 06:22:21 +0600

    configs

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,6 @@ import 'package:intl/intl.dart';
 import 'package:notes_app/src/base/app.dart';
 import 'package:notes_app/src/dependencies/injection.dart';
 import 'package:notes_app/src/services/bloc_observer.dart';
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,16 +13,6 @@ Future<void> main() async {
   Bloc.observer = GlobalBlocObserver();
 
   Intl.defaultLocale = 'ru';
-
-  await openDatabase(
-    join(await getDatabasesPath(), 'notes.db'),
-    onCreate: (db, _) {
-      return db.execute(
-        'CREATE TABLE note(id INTEGER PRIMARY KEY, text TEXT, image TEXT, createdAt TEXT)',
-      );
-    },
-    version: 1,
-  );
 
   runApp(App());
 }
